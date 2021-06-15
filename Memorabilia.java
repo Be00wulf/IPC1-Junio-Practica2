0e4d96930c41d31f2219b2483aa030813a5d59af
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-06-14 19:28:23 -0600

    Se ha creado el archivo   .gitignore

---
+++ Memorabilia.java
@@ -0,0 +1,12 @@
+//package src;
+import java.util.Scanner;
+
+public class Memorabilia {
+    //clase main
+    public static void main(String[] args) {
+        Memorabilia obj = new Memorabilia();
+ 
+    }
+
+	
+}
